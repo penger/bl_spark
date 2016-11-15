e7738f1acae0f24e923951b16d8b19c18280f41b
cloudera <gongpengllpp@sina.com>
2016-11-15 01:06:10 -0800

    delete test

--- src/main/python/Test.py
+++
@@ -1,6 +0,0 @@
-from kafka import KafkaConsumer
-consumer = KafkaConsumer('test', group_id='test', bootstrap_servers='10.201.48.106:9092')
-# consumer.assign([TopicPartition('test', 131)])
-for message in consumer:
-    print message
-
